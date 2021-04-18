14020bf626c79d540d51cb3ef907656fa125b2d1
Alejandro <k.salazar@utp.edu.co>
2021-04-18 01:01:07 -0500

    Learning to generate text

---
+++ text.py
@@ -0,0 +1,33 @@
+import sys
+import pygame
+pygame.init()
+
+width = 400
+height = 500
+surface = pygame.display.set_mode((width, height)) #surface
+pygame.display.set_caption('Pygame')
+
+## Working with the colors
+## Using a class .Color
+red = pygame.Color(255, 180, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+white = pygame.Color(255, 255, 255)
+
+# 1. Get a font
+font = pygame.font.SysFont("freesansbold", 48)
+
+# 2. Create the text 
+text = font.render('Oh! my bemo-baby', True, red) # --> surfice
+rect = text.get_rect()
+rect.center = (width // 2, height // 2)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    surface.fill(white)
+    surface.blit(text, rect)
+    pygame.display.update()
